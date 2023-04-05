69546e7b2f5595a5a81bd763fbbf811d7732fb54
elifaksoyy <32356435+elifaksoyy@users.noreply.github.com>
2023-04-05 23:19:18 +0300

    Add files via upload

---
+++ degiskenler.py
@@ -0,0 +1,31 @@
+baslik="HABERİNİZ OLSUN" #string
+vade=12 #int
+faizOrani1=1.47 #float
+faizOrani2=1.44
+faizOrani3=1.47
+
+print(baslik)
+print(type(baslik))
+print(type(  vade))
+print(type(faizOrani1))
+
+
+mesaj="Hoşgeldin"
+musteriAdi="Elif"
+musteriSoyadi="Aksoy"
+sonucMesaj=mesaj+" "+musteriAdi+" "+musteriSoyadi+"!"
+print(sonucMesaj)
+
+
+sayi1=10
+sayi2=20
+print(sayi1+sayi2)
+
+print(sonucMesaj)
+  
+
+
+
+
+
+
